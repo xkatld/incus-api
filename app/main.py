80eaedf6b0f876d9dfbc990c2a974f4baed7133f
BulaSete <36999228+xkatld@users.noreply.github.com>
2024-08-22 14:01:11 +0800

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,23 @@
+import os
+import stat
+from fastapi import FastAPI
+from app.database.db_operations import create_database
+from app.api.endpoints import router
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup_event():
+    create_database()
+    
+    # 确保 buildone.sh 有执行权限
+    app_dir = os.path.dirname(os.path.abspath(__file__))
+    buildone_path = os.path.join(app_dir, "buildone.sh")
+    current_permissions = os.stat(buildone_path).st_mode
+    os.chmod(buildone_path, current_permissions | stat.S_IEXEC)
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
